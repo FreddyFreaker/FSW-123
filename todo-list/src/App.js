2724150d1886237585405a2735b6951ffc0819c9
FreddyFreaker <jeremiah.fulkerson@bryanuniversity.edu>
2021-05-14 03:02:38 -0500

    w

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Greet from "./greeting"
+function App() {
+  return (
+  <>  
+     <h1>Hello World</h1>
+     <Greet />
+  </>
+  
+  );
+}
+
+export default App;
---
+++ todo-list/src/greeting.js
@@ -0,0 +1,22 @@
+
+function Greet() {
+    return (
+        <>
+        <h1>ToDo List</h1>
+        <ul>
+            <input type="checkbox" id="item1" name="item1" value="homework"></input>
+            <label>Homework</label>
+            <br></br>
+            <input type="checkbox" id="item1" name="item1" value="homework"></input>
+            <label>Dishes</label>
+            <br></br>
+            <input type="checkbox" id="item1" name="item1" value="homework"></input>
+            <label>Laundry</label>
+        </ul>
+        
+        </>
+    )
+
+}
+
+export default Greet
